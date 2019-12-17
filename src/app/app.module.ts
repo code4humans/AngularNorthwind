845cd6cccc9ff23bd0199d2c6e6ab055e6dfd98c
Code4Humans <smithasencios@outlook.com>
2019-12-16 22:43:23 -0500

    install ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { HomeModule } from './home/home.module';
+import {StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,8 @@ import { HomeModule } from './home/home.module';
   ],
   imports: [
     BrowserModule,
+    StoreModule.forRoot({},{}),
+    EffectsModule.forRoot([]),
     AppRoutingModule,
     SharedModule,
     HomeModule
