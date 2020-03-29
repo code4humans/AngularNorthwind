b1b228c30117bb6a35d533d2e1be1baa7a9c2ca6
smithasenciostekton <smith.asencios@tektonlabs.com>
2020-03-28 22:51:24 -0500

    update navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularNorthwind';
+  isAuthenticated = true;
+
+  login() {
+
+  }
+  logout() {
+    
+  }
 }
